963fc1f3638ee14a7697ca7e5df0b187adf27247
AhmetErenLacinbala <ahmetlacinbala@gmail.com>
2023-09-27 08:54:37 +0300

    windows has been created

---
+++ lve_window.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <string>
+namespace lve {
+    class LveWindow
+    {
+        public:
+        LveWindow(int w, int h, std::string name);
+        LveWindow(const LveWindow&) = delete;
+        LveWindow& operator=(const LveWindow&) = delete;
+        ~LveWindow();
+        bool shouldClose() { return glfwWindowShouldClose(window); }
+        private:
+            void initWindow();
+            const int width;
+            const int height;
+            std::string windowName;
+            GLFWwindow* window;
+    };
+    
+}
--- main.cpp
+++ main.cpp
@@ -1,35 +1,18 @@
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
+#include "first_app.hpp"
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
 
+#include <stdexcept>
 #include <iostream>
+#include <cstdlib>
 
-int main() {
-    glfwInit();
-
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkn window", nullptr, nullptr);
-
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::cout << extensionCount << " extensions supported\n";
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
+int main(){
+    lve::FirstApp app{};
+    try {
+        app.run();
+        std::cout<<"Hello Vulkan!"<<std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << '\n';
+        std::cout<<"Hello Vulkan!2"<<std::endl;
     }
-
-    glfwDestroyWindow(window);
-
-    glfwTerminate();
-
-    return 0;
+    return EXIT_SUCCESS;
 }
